8ef50001008557dcfdc331733dc7d699d9e5b413
Aniva <nivthehpfan@outlook.com>
2026-06-29 17:38:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if f.test() and lps.test() and psn.test() and psw.test(): print('All scripts loa
 else: print('An error ocurred.'), exit()
 
 print('Please do not use this toolkit in a way which harasses the 1990 Computer Misuse Act.\nThis toolkit only has over-simple/basic tools and their are also lots of bugs, as this is the developer\'s first time writing full code, sorry.') 
-userscmd = str(input('PeiceOfShxt>>> '))
+userscmd = str(input('chaos>>> '))
 #if anyone is here but my lonley self i have a question
 #is 6earlyhuman a good or bad music artist
 #bc ik rn theirs alot of hate on scenecore
